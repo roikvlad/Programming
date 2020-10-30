cb59868c8ec96e855c044a76d4983d7e5a905a48
unknown <you@example.com>
2020-10-30 23:18:43 +0300

    16, 18(доделываю питон)

---
+++ Practice/16/C++/16/16/16.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+	int v = 0, b = 0, k = 0, n;
+	cout << "Введите количество билетов\n";
+	cin >> n;
+	
+	char* arr = new char[n * 9];
+	char a[9]{0};
+	for (int i = 0; i < n * 9; i++)
+	{
+		cin >> arr[i];
+	}
+
+
+	for (int i = 0; i < (n * 9); i++) 
+	{
+		if ((i == 0 || i % 9 == 0) and int(arr[i]) == 97) 
+		{
+			a[0] = arr[i];
+			b += 1;
+			v = i; continue;
+		}
+		else if (i == 1 || i - v == 1) 
+		{
+			a[1] = arr[i];
+			b += 1; continue;
+		}
+		else if (i == 2 || i - v == 2) 
+		{
+			a[2] = arr[i];
+			b += 1; continue;
+		}
+		else if (i == 3 || i - v == 3) 
+		{
+			a[3] = arr[i];
+			b += 1; continue;
+		}
+		else if (i == 4 || i - v == 4 and int(arr[i]) == 53) 
+		{
+			a[4] = arr[i];
+			b += 1; continue;
+		}
+		else if (i == 5 || i - v == 5 and int(arr[i]) == 53) 
+		{
+			a[5] = arr[i];
+			b += 1;continue;
+		}
+		else if (i == 6 || i - v == 6 and int(arr[i]) == 54) 
+		{
+			a[6] = arr[i];
+			b += 1;continue;
+		}
+		else if (i == 7 || i - v == 7 and int(arr[i]) == 54) 
+		{
+			a[7] = arr[i];
+			b += 1; continue;
+		}
+		else if (i == 8 || i - v == 8 and int(arr[i]) == 49) 
+		{
+			a[8] = arr[i];
+			b +=  1;
+
+			if (b == 9) 
+			{
+				for (int h = 0; h < 9; h++) 
+				{
+					cout << a[h];
+					k = 1;
+				}
+			}
+			else 
+			{
+				k = 0; continue;
+			}
+		}
+	}
+	if (k != 1)
+	{
+		cout << -1 ;
+	}
+}
---
+++ Practice/18/C++/18/18/18.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <String.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int i, j, p=0, t=0, n, c, l = 0;;
+    double a;
+ 
+    string st;
+    cout << "Введите стоп слово\n";
+    cin >> st;
+
+    char str[] = "hallo, klempner, das, ist, fantastisch, fluggegecheimen"; 
+
+    int arr = sizeof(str) - 1;
+
+     for (int i = 0; i < arr; i++)
+    {
+        if (str[i] == char(' ') or str[i] == char(','))
+           l += 1;
+    }
+
+     a = arr - l;
+   
+    double s = 1 ;
+    c = st.length();
+    for (int i = 0; i < c ; i++)
+    {
+        int b = 0;
+        for (int j = 0; j < arr ; j++)
+        {
+            if (st[i] == str[j])
+            {
+                b += 1;
+            }
+            if (j == a - 1);
+
+        }
+        s *= b / a;
+        if (i == c - 1)
+        {
+            cout << setprecision(16) << s;
+        }
+    }
+    return 0;
+}
+
